399c1c03ed61a066875f650ff4c4dfa5d77e120e
Muhammad Bilal <bilal.soft000@gmail.com>
2024-03-14 02:08:46 +0500

    validate payload, middleware, controller

---
+++ src/routes/user.routes.ts
@@ -0,0 +1,10 @@
+import express from "express";
+import validateResource from "../middleware/validateResource";
+import { createUserSchema } from "../schema/user.schema";
+import { createUserHandler } from "../controller/user.controller";
+
+const router = express.Router();
+
+router.post("/api/users", validateResource(createUserSchema), createUserHandler);
+
+export default router;

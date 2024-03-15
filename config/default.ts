115bef25214cc55477d06515c4cf5ed96141e91e
Muhammad Bilal <bilal.soft000@gmail.com>
2024-03-15 22:57:51 +0500

    login route

--- config/default.ts
+++ config/default.ts
@@ -2,6 +2,8 @@ export default {
   port: 3000,
   dbUri: "mongodb://127.0.0.1:27017/user-api-tutorial",
   loglevel: "info",
+  accessTokenPrivateKey: "",
+  refreshTokenPrivateKey: "",
   smtp: {
     user: "uzcmv7gz46nih52h@ethereal.email",
     pass: "tBS9Hzm3nBaNdQ53BV",
--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,5 +1,9 @@
 import express from "express";
+import validateResource from "../middleware/validateResource";
+import { createSessionSchema } from "../schema/auth.schema";
+import { createSessionHandler } from "../controller/auth.controller";
 
 const router = express.Router();
+router.post("/api/sessions", validateResource(createSessionSchema), createSessionHandler);
 
 export default router;
